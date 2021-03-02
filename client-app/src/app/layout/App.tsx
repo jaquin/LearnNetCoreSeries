0d4321d0ca37460dd33d951fa146786ad98300ae
= <mehmetmert_92@hotmail.com>
2021-03-02 02:27:07 -0800

    End of Section 7

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,129 +1,36 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './styles.css';
 //import { ducks } from '../../demo';
 //import DuckItem from '../../DuckItem';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import { v4 as uuid } from 'uuid';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
 
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [loading,setLoading] = useState(true);
-  const [submitting,setSubmitting] =useState(false);
-
+  const { activityStore } = useStore();
+ 
   useEffect(() => {
-
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      })
-
-      setActivities(activities)
-      setLoading(false);
-    })
-  }, [])
-
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
-
-  function handleCancelSelectActivity() {
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  }
-
-  function handleFormClose() {
-    setEditMode(false);
-  }
-
-
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if(activity.id){
-      agent.Activities.update(activity).then(()=>{
-        setActivities([...activities.filter(x => x.id !== activity.id), activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      });
-    }
-    else{
-      activity.id = uuid();
-      agent.Activities.create(activity).then(()=>{
-        setActivities([...activities, activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      });
-    }
+    activityStore.loadActivities();
+  }, [activityStore])
 
   
 
-  }
+ 
 
-
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(()=>{
-      setActivities([...activities.filter(x => x.id !== id)]);
-      setSubmitting(false);
-    })
-    
-  }
-
-  if(loading) return <LoadingComponent content='Loading app '></LoadingComponent>
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app '></LoadingComponent>
 
   return (
     <Fragment>
 
-      <NavBar openForm={handleFormOpen}></NavBar>
+      <NavBar ></NavBar>
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard
-          activities={activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
-          createOrEdit={handleCreateOrEditActivity}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-        ></ActivityDashboard>
+        <ActivityDashboard></ActivityDashboard>
       </Container>
 
-
-
-      {/*<Header as = 'h2' icon='users' content = 'Reactivities' />
-         <img src={logo} className="App-logo" alt="logo" />
-        {ducks.map(duck => (
-          <DuckItem duck={duck} key = {duck.name}/>
-        )
-        )}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */ }
 
     </Fragment>
 
@@ -131,4 +38,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,11 @@
+import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
-
-interface Props {
-    activity: Activity | undefined;
-    closeForm: () => void;
-    createOrEdit : (activity : Activity) => void;
-    submitting:boolean;
-}
-
-export default function ActivityForm({ activity: selectedActivity, submitting, closeForm ,createOrEdit}: Props) {
+import { useStore } from '../../../app/stores/store';
+ 
+export default observer(function ActivityForm() {
+    const {activityStore} = useStore();
+    const {selectedActivity,closeForm,createActivity,updateActivity,loading } = activityStore;
 
     const initialState = selectedActivity ?? {
         id: '',
@@ -25,8 +21,7 @@ export default function ActivityForm({ activity: selectedActivity, submitting, c
 
     function handleSubmit()
     {
-        createOrEdit(activity);
-        //console.log(activity);
+        activity.id ? updateActivity(activity) : createActivity(activity);
     }
 
 
@@ -46,9 +41,9 @@ export default function ActivityForm({ activity: selectedActivity, submitting, c
                 <Form.Input type='date' placeholder="Date" value={activity.date} name='date' onChange={handleInputChange} />
                 <Form.Input placeholder="City" value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder="Venue" value={activity.venue} name='venue' onChange={handleInputChange} />
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
+                <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' negative type='button' content='Cancel' />
             </Form>
         </Segment>
     );
-}
+});
